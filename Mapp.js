f669cee04f4c9fc196b910a9378dd48bf23e3bfa
mdn <marco.denittis@synesthesia.it>
2018-01-18 18:32:20 +0100

    mapbox play

--- Mapp.js
+++ Mapp.js
@@ -6,25 +6,70 @@ import MapboxGL from '@mapbox/react-native-mapbox-gl';
 MapboxGL.setAccessToken("pk.eyJ1IjoibWRuc3luIiwiYSI6ImNqYjJlazI1bjJhejEycW8xdHlqMWRyaDYifQ.ulhUqSzC6RBTMfb1YNxrug");
 
 
+const l = console.log.bind(console);
+
+const center = {
+  coords: [7.65, 45.07],
+  zoomLevel: 10.5,
+};
+
+const icoUrl = 'https://findicons.com/files/icons/1700/2d/512/star.png';
+
+const radius= 0.2;
+
+const nodes = [1, 2, 3, 4, 5,6,7,8,9].map( id => ({
+  id,
+  coords: [
+    center.coords[0] + Math.random() * radius - (radius / 2),
+    center.coords[1] + Math.random() * radius - (radius / 2),
+  ],
+}));
+
+
+console.log(nodes);
 
 export default class Mapp extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <MapboxGL.MapView
-          styleURL={'mapbox://styles/mdnsyn/cjb3i11b2fbvi2ss869rss06y'}
+          styleURLa={'mapbox://styles/mdnsyn/cjb3i11b2fbvi2ss869rss06y'}
           ref={(ref) => this.map = ref}
+          centerCoordinate={center.coords}
+          zoomLevel={center.zoomLevel}
           showUserLocation={false}
           pitchEnabled={false}
           rotateEnabled={false}
           logoEnabled={false}
           compassEnabled={false}
-          onRegionDidChange={null}
+          onRegionDidChange={l.bind(null,'onRegionDidChange')}
           style={styles.map}
-          onDidFinishLoadingMap={null}
+          onDidFinishLoadingMap={l.bind(null,'onDidFinishLoadingMap')}
         >
+
+
+
+          {nodes.map(v => {
+
+            return (<MapboxGL.ShapeSource
+              key={`vehicle-${v.id}`}
+              id={`vehicleSource-${v.id}`}
+              shape={MapboxGL.geoUtils.makePoint(v.coords)}
+              itemId={v.id}
+
+            >
+                {/*<MapboxGL.CircleLayer id={`b-${v.id}`} style={mapStyles.nodeBorder}/>*/}
+              <MapboxGL.CircleLayer id={`bg-${v.id}`} style={mapStyles.nodeBackground} />
+
+              <MapboxGL.SymbolLayer id={`i-${v.id}`}  style={[mapStyles.nodeIcon]}/>
+
+            </MapboxGL.ShapeSource>);
+          })}
+
         </MapboxGL.MapView>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.bottomView}>
+          <Text>-</Text>
+        </View>
       </View>
     );
   }
@@ -40,7 +85,36 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
     width: '100%',
-    borderColor: 'red',
-    borderWidth: 1,
+
+  },
+  bottomView: {
+    height: 70,
+    width: '100%',
+    borderColor: '#BBB',
+    borderTopWidth: 1,
   }
+});
+
+
+const mapStyles = MapboxGL.StyleSheet.create({
+  nodeBorder: {
+    circleStrokeWidth: 2,
+    circleRadius: 27,
+    circleStrokeColor: '#187cc0',
+  },
+  nodeBackground: {
+    circleRadius: 27,
+    circleColor: 'white',
+    circleStrokeWidth: 2,
+    circleRadius: 27,
+    circleStrokeColor: '#187cc0',
+  },
+  vehicleCallout: {
+    //iconImage: calloutIco,
+    iconSize: 0.4,
+  },
+  nodeIcon: {
+    iconImage: icoUrl,
+    iconSize: 0.04,
+  },
 });
